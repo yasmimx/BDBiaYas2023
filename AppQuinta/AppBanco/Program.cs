57b9a30f38eaf4b708620edaac845ea1cd8afb99
yasmimx <101806624+yasmimx@users.noreply.github.com>
2023-08-18 11:27:42 -0300

    18.08

---
+++ AppQuinta/AppBanco/Program.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AppBanco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection Conexao = new MySqlConnection("@Server = Localhost; database = dbAppBanco; user = root; password = 12345678");
+            Console.WriteLine("teste");
+            Console.ReadLine();
+        }
+    }
+}
+ 
