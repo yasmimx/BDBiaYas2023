6a5abb5d38dbd1e74cc6ca2f856d555a47fd1eea
yasmimx <101806624+yasmimx@users.noreply.github.com>
2023-08-21 10:43:15 -0300

    21.08

--- AppQuinta/AppBanco/Program.cs
+++ AppQuinta/AppBanco/Program.cs
@@ -12,8 +12,35 @@ namespace AppBanco
     {
         static void Main(string[] args)
         {
-            MySqlConnection Conexao = new MySqlConnection("@Server = Localhost; database = dbAppBanco; user = root; password = 12345678");
-            Console.WriteLine("teste");
+            MySqlConnection Conexao = new MySqlConnection(@"Server = Localhost;database = dbAppBanco;user = root;password = 12345678");
+            Conexao.Open();
+
+            string strInsert = "insert into tbusuario(NomeUsu, Cargo, DataNasc) values ('Yasmim', 'Aluno', '2005/08/05')";
+            MySqlCommand ObjCommandI = new MySqlCommand(strInsert, Conexao);
+            ObjCommandI.ExecuteNonQuery();
+
+            string strUpdate = "update from tbusuario set";
+            MySqlCommand ObjCommandUp = new MySqlCommand(strUpdate, Conexao);
+            ObjCommandUp.ExecuteNonQuery();
+
+            
+
+            
+
+            string strSelect = "SELECT * FROM tbusuario;";
+            MySqlCommand ObjCommandS = new MySqlCommand(strSelect, Conexao);
+
+
+            MySqlDataReader leitor = ObjCommandI.ExecuteReader();
+
+            while (leitor.Read())
+            {
+                Console.WriteLine("Código = {0} | Nome = {1}  | Cargo = {2}  | Nascimento = {3}",
+                                   leitor["IdUsu"], leitor["NomeUsu"], leitor["Cargo"], leitor["DataNasc"]);
+            }
+            leitor.Close();
+
+
             Console.ReadLine();
         }
     }
